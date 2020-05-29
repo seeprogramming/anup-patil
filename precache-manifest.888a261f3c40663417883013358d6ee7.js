881b66d8b6fbab19e1add9a5f3166cd3fa0ab2ea
sagarshelke <sagarshelke18@gmail.com>
2020-05-29 21:58:34 +0530

    Updates

---
+++ precache-manifest.888a261f3c40663417883013358d6ee7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1973894094283a8fc2212467d40d6de5",
+    "url": "/seeprogramming/anup-patil-portfolio/index.html"
+  },
+  {
+    "revision": "039141098fcbcdf5b741",
+    "url": "/seeprogramming/anup-patil-portfolio/static/css/main.a55ae306.chunk.css"
+  },
+  {
+    "revision": "65df56adfc6dbf747fcd",
+    "url": "/seeprogramming/anup-patil-portfolio/static/js/2.098145df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seeprogramming/anup-patil-portfolio/static/js/2.098145df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039141098fcbcdf5b741",
+    "url": "/seeprogramming/anup-patil-portfolio/static/js/main.e122f7bd.chunk.js"
+  },
+  {
+    "revision": "48c2b7a4970a6de3b004",
+    "url": "/seeprogramming/anup-patil-portfolio/static/js/runtime-main.ddafb8d9.js"
+  },
+  {
+    "revision": "19efcc6f41a7988685f2cb5ffb41606b",
+    "url": "/seeprogramming/anup-patil-portfolio/static/media/about_me.19efcc6f.png"
+  },
+  {
+    "revision": "fa442798d3caca82dfccbd3d749790a6",
+    "url": "/seeprogramming/anup-patil-portfolio/static/media/form_section.fa442798.png"
+  },
+  {
+    "revision": "9f054c1fa1a03510ec0e5bedf9a0c2fb",
+    "url": "/seeprogramming/anup-patil-portfolio/static/media/red-moon.9f054c1f.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/seeprogramming/anup-patil-portfolio/precache-manifest.888a261f3c40663417883013358d6ee7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/seeprogramming/anup-patil-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
